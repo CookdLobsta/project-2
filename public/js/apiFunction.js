c0b42dbca58f4f8124cac84ae0fe22ba4926b269
Connor Wilson <connormwilson@gmail.com>
2018-11-12 21:42:35 -0700

    removed href & moved script tag

--- public/js/apiFunction.js
+++ public/js/apiFunction.js
@@ -1,80 +1,78 @@
-$(document).on("click",".submitToy", function(event){
-	event.preventDefaunt();
-	console.log("test");
-});
+$(document).on("click","#submitToy", function(event){
+	var count;
+		event.preventDefault();
+		console.log("test");
+
+		$("#walmartDiv").empty()
+		search = $(".searchInput").val()
+		count = 1
+		walmartCall(search, count)
+	});
+		
+	function walmartCall() {
+		$.ajax({
+			url: `http://api.walmartlabs.com/v1/search?`,
+			method: "GET",
+			data: {
+				"query": search,
+				"format": "json",
+				"apiKey": "p3dsfmf2vhm67rj2ed4xhhaa",
+				"start": count,
+				"numItems": 1
+			}
+		}).then(function (response) {
+			console.log(response);
+			for (i = 0; i < response.items.length; i++) {
+				var card = $("<div>")
+				card.attr({
+					class: "card d-inline-block m-2",
+					style: "width: 100%, margin: 0; margin-top: 0; margin-bottom: 0;"
+				});
+			}; // end for loop
+
+				var image = $("<img>")
+				image.attr({
+					src: response.items[i].mediumImage,
+					class: "card-img-top"
+				});
+					var cardBody = $("<div>")
+					cardBody.attr("class", "card-body")
+
+					var name = $("<h5>")
+					name.attr("class", "card-title")
+					name.append(response.items[i].name)
+					cardBody.append(name)
+
+					msrp = "$" + response.items[i].msrp
+					walmartPrice = response.items[i].salePrice;
+					walmartApiReturned = true;
+					if (msrp === "$undefined")
+						msrp = ""
+
+					var name = $("<h5>")
+					name.attr("class", "card-title")
+					name.append("$" + response.items[i].salePrice + " <s>" + msrp + "</s>")
+					cardBody.append(name)
+					
+					var name = $("<h5>")
+					name.attr("class", "card-title")
+					name.append("UPC: " + response.items[i].upc)
+					cardBody.append(name)
+					card.append(image)
+					card.append(cardBody)
+					
+					$("#walmartDiv").append(card)
+					
+		}); // end .then function
 
 
 
 
-// var count;
 
-	// $(document).on("click",".submitToy", function () {
-	// 	event.preventDefault();
-	// 	console.log("test");
-	// 	$("#walmartItemInfo").empty()
-	// 	search = $(".searchInput").val()
-	// 	count = 1
-	// 	walmartCall(search, count)
-	// })
 
-	// function walmartCall() {
-	// 	$.ajax({
-	// 		url: `http://api.walmartlabs.com/v1/search?`,
-	// 		method: "GET",
-	// 		data: {
-	// 			"query": search
-	// 			, "format": "json"
-	// 			, "apiKey": "p3dsfmf2vhm67rj2ed4xhhaa"
-	// 			, "start": count
-	// 			, "numItems": 1
-	// 		}
-	// 	}).then(function (response) {
-	// 		console.log(response);
 
-	// 		for (i = 0; i < response.items.length; i++) {
-	// 			var card = $("<div>")
-	// 			card.attr({
-	// 				class: "card d-inline-block m-2",
-	// 				style: "width: 100%, margin: 0; margin-top: 0; margin-bottom: 0;"
-	// 			})
 
-	// 			var image = $("<img>")
-	// 			image.attr({
-	// 				src: response.items[i].mediumImage,
-	// 				class: "card-img-top"
-	// 			})
 
-	// 			var cardBody = $("<div>")
-	// 			cardBody.attr("class", "card-body")
-
-	// 			var name = $("<h5>")
-	// 			name.attr("class", "card-title")
-	// 			name.append(response.items[i].name)
-	// 			cardBody.append(name)
-
-	// 			msrp = "$" + response.items[i].msrp
-	// 			walmartPrice = response.items[i].salePrice;
-	// 			walmartApiReturned = true;
-
-	// 			if (msrp === "$undefined")
-	// 			msrp = ""
-
-	// 			var name = $("<h5>")
-	// 			name.attr("class", "card-title")
-	// 			name.append("$" + response.items[i].salePrice + " <s>" + msrp + "</s>")
-	// 			cardBody.append(name)
-
-	// 			var name = $("<h5>")
-	// 			name.attr("class", "card-title")
-	// 			name.append("UPC: " + response.items[i].upc)
-	// 			cardBody.append(name)
-	// 			card.append(image)
-	// 			card.append(cardBody)
-
-	// 			$("#walmartItemInfo").append(card)
-	// 		}
-	// 	})
-	// };
 
 	// $('submit').on('click', event =>{
 	// 	event.preventDefault();
