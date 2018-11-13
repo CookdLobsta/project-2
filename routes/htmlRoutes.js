d9c337a4cd558620c3bf9ddb67d8eded4f4b85b3
TaylorRoy <tbroy@hotmail.com>
2018-11-12 21:36:52 -0700

    splash is / and handlebars, and routing is underway

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,15 +1,20 @@
 var db = require('../models');
 
+
 module.exports = function (app) {
+	app.get("/", function(req, res) {
+		console.log("hit");
+		res.render('splash');
+	  });
 	// Load index page
-	app.get('/', function (req, res) {
-		db.Example.findAll({}).then(function (dbExamples) {
-			res.render('index', {
-				msg: 'Welcome!',
-				examples: dbExamples
-			});
-		});
-	});
+	// app.get('/', function (req, res) {
+	// 	db.Example.findAll({}).then(function (dbExamples) {
+	// 		res.render('index', {
+	// 			msg: 'Welcome!',
+	// 			examples: dbExamples
+	// 		});
+	// 	});
+	// });
 
 	// Load example page and pass in an example by id
 	app.get('/example/:id', function (req, res) {
@@ -22,6 +27,6 @@ module.exports = function (app) {
 
 	// Render 404 page for any unmatched routes
 	app.get('*', function (req, res) {
-		res.render('404');
+		// res.render('404');
 	});
 };
