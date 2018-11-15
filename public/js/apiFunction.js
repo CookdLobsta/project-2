a7b16cecf227e88e963b00751d1db2fd5aa1472f
Connor Wilson <connormwilson@gmail.com>
2018-11-14 21:35:38 -0700

    api features

--- public/js/apiFunction.js
+++ public/js/apiFunction.js
@@ -1,63 +1,34 @@
 $(document).on("click","#submitToy", function(event){
-	var count;
-		event.preventDefault();
-		console.log("test");
- 		$("#walmartDiv").empty()
-		search = $(".searchInput").val()
-		count = 1
-		walmartCall(search, count)
-	});
-		
-	function walmartCall() {
+	event.preventDefault();
+	console.log("Retrieving Data...");
+
+	
+	function walmartItem(search) {
+		var search = $("#searchInput").val().trim();
+		console.log(search);
+		var walmartRequest = "http://api.walmartlabs.com/v1/search?query="+search+"&format=json&apiKey=g5eyb7eyu32n6atjb8k9eq9x";
 		$.ajax({
-			url: `http://api.walmartlabs.com/v1/search?`,
-			method: "GET",
-			data: {
-				"query": search,
-				"format": "json",
-				"apiKey": "p3dsfmf2vhm67rj2ed4xhhaa",
-				"start": count,
-				"numItems": 1
+			type: 'GET',
+			url: walmartRequest
+		})
+		.then(function (data) {
+			console.log('data', data);
+			// walmartRequest, function (error, response, json) {
+			if(!error && response.statusCode === 200) {
+				var itemInfo = JSON.parse(json);
+				console.log();
+				let output = {
+					"image":itemInfo.items.mediumImage,
+					"upc":itemInfo.items.upc,
+					"sale price":itemInfo.items.salePrice,
+					"name":itemInfo.items.name
+				}
+				console.log(output);
+			} else {
+				console.log("You broke it!: " + error);
+				return;
 			}
-		}).then(function (response) {
-			console.log(response);
-			for (i = 0; i < response.items.length; i++) {
-				var card = $("<div>")
-				card.attr({
-					class: "card d-inline-block m-2",
-					style: "width: 100%, margin: 0; margin-top: 0; margin-bottom: 0;"
-				});
-			}; // end for loop
- 				var image = $("<img>")
-				image.attr({
-					src: response.items[i].mediumImage,
-					class: "card-img-top"
-				});
-					var cardBody = $("<div>")
-					cardBody.attr("class", "card-body")
- 					var name = $("<h5>")
-					name.attr("class", "card-title")
-					name.append(response.items[i].name)
-					cardBody.append(name)
- 					msrp = "$" + response.items[i].msrp
-					walmartPrice = response.items[i].salePrice;
-					walmartApiReturned = true;
-					if (msrp === "$undefined")
-						msrp = ""
- 					var name = $("<h5>")
-					name.attr("class", "card-title")
-					name.append("$" + response.items[i].salePrice + " <s>" + msrp + "</s>")
-					cardBody.append(name)
-					
-					var name = $("<h5>")
-					name.attr("class", "card-title")
-					name.append("UPC: " + response.items[i].upc)
-					cardBody.append(name)
-					card.append(image)
-					card.append(cardBody)
-					
-					$("#walmartDiv").append(card)
-					
 		})
 	};
-		// end .then function
+	walmartItem();
+});
