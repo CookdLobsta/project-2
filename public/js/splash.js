13ba906dc548ac4219b04786862309717a1397c8
Dennis The Menace <sdennisp25@hotmail.com>
2018-11-15 19:40:30 -0700

    cleaned up handlebars

--- public/js/splash.js
+++ public/js/splash.js
@@ -1,46 +1,37 @@
-<<<<<<< HEAD
-=======
+$(document).ready(function() {
+  // $("#myBtn").click(function () {
+  // $("#myModal").modal();
 
-<<<<<<< HEAD
-			 
->>>>>>> master
-$(document).ready(function () {
-	// $("#myBtn").click(function () {
-		// $("#myModal").modal();
-		
-	// });
-=======
->>>>>>> master
+  // });
+  // $(document).ready(function () {
+  // $("#myBtn").click(function () {
+  // 	$("#myModal").modal();
+  // });
+  $(function() {
+    $(document).on("click", "#submit", function(event) {
+      event.preventDefault();
+      console.log("hit");
+      var name = $("#username")
+        .val()
+        .trim();
+      console.log(name);
 
-// $(document).ready(function () {
-// $("#myBtn").click(function () {
-// 	$("#myModal").modal();
-// });
-$(function () {
-	$(document).on("click", "#submit", function (event) {
-		event.preventDefault();
-		console.log("hit");
-		var name = $("#username").val().trim();
-		console.log(name);
+      var userName = {
+        user_name: name
+      };
 
-		var userName = {
-			user_name: name
-		};
-
-		// Send the POST request.
-		$.ajax("/money_manager_post", {
-			type: "POST",
-			contentType: 'application/json',
-			data: JSON.stringify(userName)
-		}).then(
-			function (res) {
-				console.log("created new user", res);
-				 window.location = res;
-			}
-		);
-	});
-})
-// });
+      // Send the POST request.
+      $.ajax("/money_manager_post", {
+        type: "POST",
+        contentType: "application/json",
+        data: JSON.stringify(userName)
+      }).then(function(res) {
+        console.log("created new user", res);
+        window.location = res;
+      });
+    });
+  });
+});
 
 
 // modules.export = nameData;
