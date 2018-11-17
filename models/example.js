82ac2255f9cc9a3dd238d9913f926ad5eb295866
Connor Wilson <connormwilson@gmail.com>
2018-11-17 13:59:55 -0700

    changes

--- models/example.js
+++ models/example.js
@@ -3,13 +3,8 @@ module.exports = function (sequelize, DataTypes) {
 		user_name: DataTypes.STRING,
 		user_balance: DataTypes.DECIMAL(10,2),
 		goal_name: DataTypes.STRING,
-<<<<<<< HEAD
-		goal_upc: DataTypes.INTEGER,
-		goal_price: DataTypes.INTEGER,
-=======
 		goal_upd: DataTypes.INTEGER,
 		goal_price: DataTypes.DECIMAL(10,2),
->>>>>>> master
 		createdAt: {
 			allowNull: false,
 			type: DataTypes.DATE,
