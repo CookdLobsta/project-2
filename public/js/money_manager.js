d2d94a6c3e58d915ca33b9f432a7ff7e054b8436
cyrus <cyruskeo99@gmail.com>
2018-11-19 14:46:09 -0700

    saved goal img and price to db displayed goal img to page, caculated difference between userBalance and goalPrice then displayed it on page, got differecne between goalprice and userBalance and turned into a percentage to update progress bar, fixed caluclator (to fixed is not a function err)

--- public/js/money_manager.js
+++ public/js/money_manager.js
@@ -2,91 +2,59 @@
 var total = 0;
 var saveTotal = 0;
 var userID = 0;
+var amountTillGoal = 0;
+var progressBarFill;
 
 var urlLocation = window.location;
-console.log(JSON.stringify(urlLocation.search).split("=")[1]);
+// console.log(JSON.stringify(urlLocation.search).split("=")[1]);
 var urlNum = parseInt(JSON.stringify(urlLocation.search).split("=")[1]);
-console.log(urlNum);
-console.log("url", urlLocation);
+// console.log(urlNum);
+// console.log("url", urlLocation);
 
 $(document).ready(function(){
 	$.get("/api/get_balance/" + urlNum, function(res){
 
-		console.log(res);
+		// console.log(res);
 		$("#welcome").text("Welcome " + res.user_name);
 		total = res.user_balance
 		saveTotal = res.user_balance
 		$("#user_total").text(res.user_balance);
-		$
-	})
-})
+		$(".balance").text(res.user_balance)
+		$(".topImage").attr('src', res.goal_img);
+		amountTillGoal = res.goal_price - res.user_balance;
+		$(".amountLeft").text(amountTillGoal)
+		progressBarFill = Math.floor((res.user_balance / res.goal_price) * 100);
+		$("#progress_bar").css({width : progressBarFill + "%"})
+		
+	});
+});
 
 //onload get user balance, put on page
 $(document).on("click", ".money", function(event) {
   // alert("hit");
   var money = $(this).attr("data-value");
-  console.log(money);
-  (total += parseFloat(money)).toFixed(2);
-  console.log(total);
+	// console.log(money);
+	var totalInt = parseFloat(total)
+	
+  total = (totalInt += parseFloat(money));
+  // console.log(total);
 
   $(".newTotal").html(total.toFixed(2));
 
   var audio = new Audio("../images/chaching.mp3");
   audio.play();
-
-  var userName = {
-		user_balance: total
-  };
-
-  $.ajax("/money_manager_post", {
-    type: "PUT",
-    contentType: "application/json",
-    data: JSON.stringify(userName)
-  }).then(function(res) {
-    console.log("added money", res);
-    window.location = res;
-  });
 });
-
-// Send the POST request.
-// $.ajax("/money_manager_post", {
-// 	type: "POST",
-// 	contentType: 'application/json',
-// 	data: JSON.stringify(userName)
-// }).then(
-// 	function (res) {
-// 		console.log("added money", res);
-// 		window.location = res;
-// 	}
-// );
-
 
 // post user_balance to database
 
 $(document).on("click", ".submitTotal", function (event) {
 	event.preventDefault();
 	var userTotal = parseFloat($(".newTotal").text());
-	// console.log("total "+ userTotal);
-	// console.log(typeof(userTotal));
-	// var userMoney = {
-	// 	user_balance: userTotal 
-	// } 
-
-	// $.ajax("/money_manager_money", {
-	// 	type: "POST",
-	// 	contentType: 'application/json',
-	// 	data: JSON.stringify(userMoney)
-	// }).then(
-	// 	function (res) {
-	// 		console.log("added money", res);
-	// 		window.location = res;
-	// 	}
-	// );
 
 	var userMoney = {
 		user_balance: userTotal,
 		idNumber: urlNum
-	}
+	};
 
 	$.ajax({
 		method: "PUT",
@@ -95,26 +63,30 @@ $(document).on("click", ".submitTotal", function (event) {
 		data: JSON.stringify(userMoney)
 	  }).then(
 			function (res) {
-				console.log("added money to john", res);
+				// console.log("added money to john", res);
 				window.location = res;
-			})
-})
+			});
+});
 
-//$(document).on("click", ".submitTotal", function(event) {
-//  var userTotal = parseFloat($(".newTotal").text());
-  // console.log("total "+ userTotal);
-  // console.log(typeof(userTotal));
-//  var userMoney = {
-//    user_balance: userTotal
-//  };
+$(document).on("click", ".saveItem", function (event) {
+	event.preventDefault();
+	var item_price = itemInfo.salePrice;
+	var item_img = itemInfo.mediumImage;
 
- // $.ajax("/money_manager_money", {
- //   type: "POST",
- //   contentType: "application/json",
- //   data: JSON.stringify(userMoney)
- // }).then(function(res) {
- //   console.log("added money", res);
- //   window.location = res;
- // });
-//});
+	var userGoal = {
+		goal_price: item_price,
+		goal_img:item_img,
+		idNumber: urlNum
+	};
 
+	$.ajax({
+		method: "PUT",
+		url: "/api/goal",
+		contentType: "application/json",
+		data: JSON.stringify(userGoal)
+	  }).then(
+			function (res) {
+				// console.log("added money to john", res);
+				window.location = res;
+			});
+});
